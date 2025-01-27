eaf44212298ca5f330c612c816e8f7f4b4401bea
GustavoKobielski <gust.kobielski@gmail.com>
2025-01-27 16:32:32 -0300

    fix: routing configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import Home from './pages/Home/Home'; // Pagina Home
+import About from './pages/About/About'; // Pagina About
+import Contact from './pages/Contact/Contact'; // Pagina Contact
+import Shop from './pages/Shop/Shop'; // Pagina Shop
 
 function App() {
-  return <h1>dd</h1>
+  return (
+    <Router>
+      <div>
+        {/* Navbar aq */}
+
+        <Routes>
+          <Route path="/" element={<Home />} />          {/* Página Home */}
+          <Route path="/about" element={<About />} />    {/* Página About */}
+          <Route path="/contact" element={<Contact />} />{/* Página Contact */}
+          <Route path="/shop" element={<Shop />} />      {/* Página Shop */}
+        </Routes>
+
+        {/* Footer aq */}
+      </div>
+    </Router>
+  )
 }
 
 export default App
