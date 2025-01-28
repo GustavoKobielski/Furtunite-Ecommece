608e57dcd1bc16e44f539785ea88d3438d14bb18
GustavoKobielski <gust.kobielski@gmail.com>
2025-01-28 14:40:38 -0300

    src/assets/Home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,24 @@
+import style from './Home.module.css'
 
 
 const Home = () => {
-  return <h1>Pagina Home</h1>
+  return (
+    <main className={style.main}>
+      <div className={style.container}>
+        <div className={style.info_pop}>
+
+          <div className={style.content}>
+            <h2 className={style.subtitle}>New Arrival</h2>
+            <h1 className={style.title}>Discover Our New Collection</h1>
+            <p className={style.text}>Lorem Ipsum Dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+            <a className={style.buy_now}>Buy now</a>
+          </div>
+        </div>
+      </div>
+
+    </main>
+
+  )
 }
 
 export default Home;
