2183fe4a07a9496e9459f7cc74c465f8fd9e75a7
GustavoKobielski <gust.kobielski@gmail.com>
2025-01-29 16:46:38 -0300

    Feat: Shop Page

--- src/pages/Shop/Shop.tsx
+++ src/pages/Shop/Shop.tsx
@@ -1,7 +1,47 @@
 
 
+import Background_main from '../../assets/Shop/background_mains.svg'
+
+// Icons
+import IconSearch from '../../assets/Icons/Shop/system-uicons_filtering.svg'
+import SelectIcon from '../../assets/Icons/Shop/ci_grid-big-round.svg'
+import IconList from '../../assets/Icons/Shop/bi_view-list.svg'
+
 const Shop = () => {
-  return <h1>Pagina Shop</h1>
+  return (
+    <main>
+      <div className='h-auto'>
+        <section className="w-full">
+          <img className='w-full object-cover h-full' src={Background_main} alt="" />
+
+          <div className='bg-[#F9F1E7] w-full h-32'>
+            <div className='flex justify-between py-9 px-32 items-center'>
+              <div className='flex items-center gap-6'>
+                <img src={IconSearch}/>
+                <p>Filter</p>
+
+
+                <img src={SelectIcon} alt="" />
+                <img src={IconList} alt="" />
+
+                {/* search results */}
+              </div>
+
+              <div className='flex items-center gap-6'>
+                <p>Show</p>
+                <span>16</span>
+
+                <p>Short by</p>
+                <span>Default</span>
+              </div>
+
+            </div>
+          </div>
+        </section>
+      </div>
+    </main>
+
+  )
 }
 
 export default Shop;
